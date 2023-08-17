737997d98632deea6d10a21b1d3f0ef1435cc7db
Ajay Agrawal <ajay.agrawal@acquia.com>
2023-08-17 21:11:37 +0530

    added source

--- post_sanity_check.sh
+++ post_sanity_check.sh
@@ -30,90 +30,6 @@ check_high_load() {
 
 
 
-scw ()
-{
-    [ "$#" -lt 1 -o "$#" -gt 2 -o "$1" = '-h' -o "$1" = '--help' ] && echo "Usage: ${FUNCNAME[0]} SITE [REGION|BAL]" && echo -n "     Second argument is optional and specifies AWS region in " && echo "which to do the checks for a multiregion site or a balancer from " && echo "from which to do the checks" && return 2;
-    local SITE=$1;
-    if [[ -n $( ah-site list $SITE -w provider=8701 ) ]]; then
-        printf 'Error - site-checkwebs does not work on Polaris sites\n';
-        return 2;
-    fi;
-    local RETURN_CODE BAL_WITH_REGION REGION REGION_OPT BAL;
-    local CURL_OPTS CURL_COMMAND ERR_OPTS WEB WEBS URI;
-    local SITE_JSON_FILE SITE_NAME SITE_VHOST_PORT SITE_E2E_ID;
-    declare -a REGIONS;
-    declare -a BAL_RECORD;
-    declare -a SITES;
-    SITE_JSON_FILE=${OPSTMP}/${FUNCNAME[0]}_$(date +%s).json;
-    ah-site get $SITE --format=json > $SITE_JSON_FILE;
-    SITE_NAME=$(cat ${SITE_JSON_FILE} | jq -r '.name');
-    SITE_VHOST_PORT=$(cat ${SITE_JSON_FILE} | jq -r '.config_settings.vhost.port // 80');
-    SITE_E2E_ID=$(cat ${SITE_JSON_FILE} |jq -r '.e2e_key_pair_id');
-    if [ $SITE_E2E_ID != "null" ]; then
-        echo -e "\e[33m$SITE_NAME is an e2e enabled site, ${FUNCNAME[0]} only works for non e2e enabled sites";
-        rm ${SITE_JSON_FILE};
-        return 0;
-    fi;
-    if [[ -n ${2} ]]; then
-        REGIONS=(ap-northeast-1 ap-northeast-2 ap-south-1 ap-southeast-1 ap-southeast-2 ca-central-1 eu-central-1 eu-north-1 eu-west-1 eu-west-2 eu-west-3 sa-east-1 us-east-1 us-east-2 us-west-1 us-west-2);
-        if $(contains $2  ${REGIONS[@]}); then
-            REGION_OPT="ec2_region=$2";
-        else
-            SITES=($(ah-site list on:$2|paste -sd" "));
-            if [[ ${#SITES[@]} -eq 0 ]]; then
-                echo "$2 does not exist or does not contain any sites";
-                return 1;
-            fi;
-            if $(contains ${SITE_NAME} ${SITES[@]}); then
-                BAL_RECORD=($(ah-server list $2 -c ec2_region | tr -d ","));
-            else
-                echo "$2 does not contain site '$SITE', please specify a bal that belongs to site '$SITE'";
-                return 1;
-            fi;
-        fi;
-    fi;
-    CURL_OPTS="-H 'Host: ${SITE_NAME}.${FIELDS_STAGE}.${FIELDS_SITE_DOMAIN}'";
-    CURL_OPTS+=" -m 15 -s -H 'X-Acquia-Monitoring: ${AMPSK}'";
-    ERR_OPTS="-w '%{http_code} %{time_total} %{url_effective}\\n'";
-    RETURN_CODE=1;
-    declare -A BAL_WITH_REGION;
-    if [[ ${#BAL_RECORD[@]} -eq 0 ]]; then
-        eval "$(ah-server  list site:${SITE_NAME} -w ${REGION_OPT}       typeINbal status=0 -c ec2_region tags      | grep -v testbal       | sed 's/,//g'      | awk '{print $1,$2}'      | sort -u -k 2,2      | awk '{print "BAL_WITH_REGION["$2"]="$1}')";
-    else
-        BAL_WITH_REGION["${BAL_RECORD[1]}"]="${BAL_RECORD[0]}";
-    fi;
-    if [[ -z "${SITE_NAME}" ]] || [[ ${#BAL_WITH_REGION[@]} -eq 0 ]]; then
-        echo -n "${SITE_NAME} does not exist or does not have a ";
-        echo "balancer in the given region $2";
-        echo "Usage: ${FUNCNAME[0]} SITE [REGION]" && return $RETURN_CODE;
-    fi;
-    for REGION in "${!BAL_WITH_REGION[@]}";
-    do
-        BAL=${BAL_WITH_REGION[$REGION]};
-        echo -n "Using ${REGION} balancer ${BAL} for checks. ";
-        WEBS=($(cat ${SITE_JSON_FILE} | jq -r --arg REGION "${REGION}"  '.web_servers[] | select(.web_service_status == "2" and .ec2_region == $REGION and .site_server_status == "1000") .name'));
-        if [[ ${WEBS} == "" ]]; then
-            echo "Error: no active webs found for: ${SITE_NAME} in ${REGION}";
-            RETURN_CODE=1;
-        else
-            for WEB in ${WEBS[@]};
-            do
-                URI="$(fqual ${WEB}):${SITE_VHOST_PORT}/ACQUIA_MONITOR?site=${SITE_NAME}";
-                CURL_COMMAND+="echo -n '${WEB}: '; curl ${CURL_OPTS} ${URI} | grep success           || curl -L ${CURL_OPTS} ${ERR_OPTS} ${URI} -o /dev/null; ";
-            done;
-            echo -n "Uptime for ${BAL}: ";
-            fssh ${BAL} "uptime;${CURL_COMMAND}" 2>&1 | grep -v "Warning: Permanently added";
-            CURL_COMMAND="";
-            RETURN_CODE=0;
-        fi;
-    done;
-    rm ${SITE_JSON_FILE};
-    return $RETURN_CODE
-}
-
-
-
-
 # Check if the Input is site or Server
 is_site_or_server() {
     local input="$1"
@@ -174,7 +90,7 @@ site-sanity-check() {
 
     # Web Checks
     echo -e "Performing Web Check now ..."
-    check_output=$(scw $site | grep web)
+    check_output=$(site-checkwebs $site | grep web)
     echo -e "$check_output"
     nos_of_webs=$(esl2 $site | grep "web_rotation_status: 1000" | wc -l)
     nos_of_success=$(echo "$check_output" | tr '[:upper:]' '[:lower:]' | grep -o 'success' | wc -l)
@@ -217,5 +133,5 @@ main() {
     fi
 }
 
-
+source ~/.bashrc
 main "$1";
