d95b8005bad83fa3a97d32c04036ccee2b13c40f
Ajay Agrawal <ajay.agrawal@acquia.com>
2023-08-17 21:23:38 +0530

    file store

--- post_sanity_check.sh
+++ post_sanity_check.sh
@@ -102,9 +102,9 @@ site-sanity-check() {
 
     # Web Checks
     echo -e "Performing Web Check now ..."
-    # site-checkwebs $site >> $OPSTMP/webchecktemp$site
-    # check_output=$(cat $OPSTMP/webchecktemp$site)
-    check_output=$(site-checkwebs $site | grep web-)
+    site-checkwebs $site >> $OPSTMP/webchecktemp$site
+    check_output=$(cat $OPSTMP/webchecktemp$site)
+    # check_output=$(site-checkwebs $site | grep web-)
     echo -e "$check_output"
     nos_of_webs=$(esl2 $site | grep "web_rotation_status: 1000" | wc -l)
     nos_of_success=$(echo "$check_output" | tr '[:upper:]' '[:lower:]' | grep -o 'success' | wc -l)
@@ -147,5 +147,5 @@ main() {
     fi
 }
 
-source ~/.bashrc
+
 main "$1";
