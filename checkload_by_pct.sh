107d4267f87b790793562b4a40afaabd882d10d0
Ajay Agrawal <ajay.agrawal@acquia.com>
2023-08-18 22:13:31 +0530

    lines

--- checkload_by_pct.sh
+++ checkload_by_pct.sh
@@ -1,15 +1,13 @@
 check_high_load_by_pct() {
-    webloads_path=$1
-
-    while IFS= read -r line; do
-        echo -e "$line" | awk '{print $11}' | tr -d '%' | bc -l
-    done < $webloads_path
+    webloads="$1"
+    echo "$webloads" | while IFS= read -r line; do
+        echo "Processing line: $line"
+    done
 }
 
 
 site=$1
 echo -e "\n[ $(date) ] - Checking Load of Individual servers by Percentage on the stack now ..."
 check_output=$(site-getloadpct $site | sed '1d')
-echo -e "$check_output" > $OPSTMP/get_load_pct_$site
-load_outputs=$(check_high_load_by_pct "$OPSTMP/get_load_pct_$site")
+load_outputs=$(check_high_load_by_pct "$check_output")
 echo -e "$load_outputs"
